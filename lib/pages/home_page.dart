ca532c1ece70399f061cefee1a210fa2536e02dd
Dev2 <dev2@babylonsciences.com>
2022-01-26 15:53:03 +0100

    day 14 complete

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -40,11 +40,44 @@ class _HomePageState extends State<HomePage> {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: CatalogModel.items.isNotEmpty
-            ? ListView.builder(
+            ? GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 16,
+                    crossAxisSpacing: 16),
+                itemBuilder: (context, index) {
+                  final item = CatalogModel.items[index];
+                  return Card(
+                      clipBehavior: Clip.antiAlias,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      child: GridTile(
+                          header: Container(
+                            child: Text(
+                              item.name,
+                              style: const TextStyle(color: Colors.white),
+                            ),
+                            padding: const EdgeInsets.all(12),
+                            decoration:
+                                const BoxDecoration(color: Colors.deepPurple),
+                          ),
+                          child: Image.network(item.image),
+                          footer: Container(
+                              child: Text(
+                                item.price.toString(),
+                                style: const TextStyle(color: Colors.white),
+                              ),
+                              padding: const EdgeInsets.all(12),
+                              decoration:
+                                  const BoxDecoration(color: Colors.black))));
+                },
                 itemCount: CatalogModel.items.length,
-                itemBuilder: (context, index) => ItemWidget(
-                      item: CatalogModel.items[index],
-                    ))
+              )
+            //   ListView.builder(
+            //     itemCount: CatalogModel.items.length,
+            //     itemBuilder: (context, index) => ItemWidget(
+            //           item: CatalogModel.items[index],
+            //         ))
             : const Center(
                 child: CircularProgressIndicator(),
               ),
