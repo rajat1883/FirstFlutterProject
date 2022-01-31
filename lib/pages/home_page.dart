73a01c19926305020d2366de1f71e2c2aeedeee3
Dev2 <dev2@babylonsciences.com>
2022-02-01 00:14:17 +0100

    day 29 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,52 +1,70 @@
-import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:flutter_application_1/core/store.dart';
+import 'package:flutter_application_1/models/cart.dart';
 import 'package:flutter_application_1/models/catalog.dart';
 import 'package:flutter_application_1/utils/routes.dart';
 import 'package:flutter_application_1/widgets/home_widgets/catalog_header.dart';
 import 'package:flutter_application_1/widgets/home_widgets/catalog_list.dart';
+import 'dart:convert';
 import 'package:velocity_x/velocity_x.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
-
   @override
-  State<HomePage> createState() => _HomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  final int days = 30;
+
+  final String name = "Codepur";
+
+  final url = "https://api.jsonbin.io/b/61f86c88fb3ece3ad7cf21f1";
+
   @override
   void initState() {
     super.initState();
     loadData();
   }
 
-  void loadData() async {
-    await Future.delayed(const Duration(seconds: 2));
-    var catalogJson = await rootBundle.loadString("assets/files/catalog.json");
-    var productData = jsonDecode(catalogJson)["products"];
-    CatalogModel.items =
-        List.from(productData).map<Item>((item) => Item.fromMap(item)).toList();
+  loadData() async {
+    await Future.delayed(Duration(seconds: 2));
+    // final catalogJson =
+    //     await rootBundle.loadString("assets/files/catalog.json");
+
+    final response = await http.get(Uri.parse(url));
+    final catalogJson = response.body;
+    final decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+    CatalogModel.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
     setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-    // final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
-
+    final _cart = (VxState.store as MyStore).cart;
     return Scaffold(
-        backgroundColor: Theme.of(context)
-            .canvasColor, // with velocity x package use context.canvasColor
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            Navigator.pushNamed(context, MyRoutes.cartRoute);
-          },
-          backgroundColor: context.theme.buttonColor,
-          child: const Icon(
-            CupertinoIcons.cart,
-            color: Colors.white,
-          ),
+        backgroundColor: context.canvasColor,
+        floatingActionButton: VxBuilder(
+          mutations: {AddMutation, RemoveMutation},
+          builder: (context, store, status) => FloatingActionButton(
+            onPressed: () => Navigator.pushNamed(context, MyRoutes.cartRoute),
+            backgroundColor: context.theme.buttonColor,
+            child: Icon(
+              CupertinoIcons.cart,
+              color: Colors.white,
+            ),
+          ).badge(
+              color: Vx.gray200,
+              size: 22,
+              count: _cart.items.length,
+              textStyle: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              )),
         ),
         body: SafeArea(
           child: Container(
@@ -55,10 +73,10 @@ class _HomePageState extends State<HomePage> {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 CatalogHeader(),
-                if (CatalogModel.items.isNotEmpty)
+                if (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
                   CatalogList().py16().expand()
                 else
-                  const CircularProgressIndicator().centered().py16().expand()
+                  CircularProgressIndicator().centered().expand(),
               ],
             ),
           ),
